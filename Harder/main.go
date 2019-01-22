c5d59d2081ebabf14fc78d56e038c8f53ab4884d
tanakorn1994 <tanakorn.su@tnis.com>
2019-01-23 00:26:23 +0700

    create func find Tendem for size 3 and set ig nore result

--- Harder/main.go
+++ Harder/main.go
@@ -4,10 +4,40 @@ import (
 	"fmt"
 )
 
+var resultMap = make(map[string] []int)
+
 func main(){
-    
+    Tandem("GAGGCATCATCATCATCATTGCC")
 }
 
-func Tandem(){
-	fmt.Println("X")
+func Tandem(input string){
+	
+
+	for i := range input {
+		if i + 3 < len(input) + 1 {
+			frameValue := input[i:i+3]
+			if val, ok := resultMap[frameValue]; ok {
+				if val[0] != -1 {
+					resultMap[frameValue] = append(resultMap[frameValue],i)
+					setIgnoreValue(frameValue)
+				}
+			}else{
+				resultMap[frameValue] = []int{i}
+				setIgnoreValue(frameValue)
+			}
+			// fmt.Println(frameValue)
+			
+		}
+	}
+	fmt.Println(resultMap)
+	
+}
+
+func setIgnoreValue(key string){
+	ignoreKey := key
+	for len(ignoreKey) + len(key) <= 10 {
+		ignoreKey += key
+		resultMap[ignoreKey] = []int{-1}
+	}
+		
 }
